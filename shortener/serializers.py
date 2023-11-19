bf47aae6c6ba90e185b77ff2940bbf66533e23b9
Mahdiye Darjazini <mahdiye.darjazini@gmail.com>
2023-11-19 14:44:51 +0100

    Fix attribute name of Meta class.

--- shortener/serializers.py
+++ shortener/serializers.py
@@ -7,10 +7,10 @@ class URLShortenerSerializer(ModelSerializer):
     class Meta:
         model = ShortenedURL
         fields = ['url', 'slug']
-        readonly_fields = ['slug']
+        read_only_fields = ['slug']
 
 
 class URLStatisticsSerializer(ModelSerializer):
     class Meta:
         model = ShortenedURL
-        readonly_fields = ['visitor_count']
+        read_only_fields = ['visitor_count']
